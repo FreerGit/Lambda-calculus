bee80a8942d9e099481811faf58875d71eff2eac
FreerGit <fredrik-98@live.se>
2023-10-11 19:24:36 +0200

    chore: project setup
    - dune init
    - nix-shell

---
+++ shell.nix
@@ -0,0 +1,31 @@
+let
+  nixpkgs-sources =
+    builtins.fetchTarball
+      https://github.com/nix-ocaml/nix-overlays/archive/master.tar.gz;
+  pkgs = import nixpkgs-sources { };
+  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_0;
+
+in
+pkgs.mkShell {
+  # build tools
+  
+  nativeBuildInputs = with pkgs; [ dune-release ocamlformat inotify-tools ];
+  # dependencies
+  buildInputs = 
+    with ocamlPackages;
+     [
+      ocaml
+      dune_3
+      ocaml-lsp
+
+      findlib
+      merlin
+      core
+      utop
+      
+      iter
+      ppx_deriving
+      ppx_expect
+      
+  ];
+}
